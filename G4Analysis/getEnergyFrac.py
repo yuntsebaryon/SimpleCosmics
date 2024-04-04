41c4fc48491899b7013f5bbd672d823c2a8ca4a3
Yun-Tse Tsai <yuntsebaryon@gmail.com>
2024-04-04 08:50:08 -0700

    code update for the v2 samples and the related notebooks

--- G4Analysis/getEnergyFrac.py
+++ G4Analysis/getEnergyFrac.py
@@ -11,13 +11,15 @@ def particleLengths(particles): return particles.apply(stepTotalLength)
 
 if __name__ == "__main__":
 
-    indir = '/Users/yuntse/data/coherent/preLArTPC/geant4/nueArCCoutFiducial'
-    inPrefix = 'nueArCC_sns_yDir'
-    outcsv = '/Users/yuntse/data/coherent/preLArTPC/analysis/EFrac/nueArCCoutFiducialEFrac.csv'
+    indir = '/Users/yuntse/data/coherent/preLArTPC/geant4/CR2500Kv2'
+    inPrefix = 'cosmic'
+    outcsv = '/Users/yuntse/data/coherent/preLArTPC/analysis/EFracv2/cosmicEFrac.csv'
 
-    nFiles = 40
+    nFiles = 2500
 
-    columns = [ 'Run', 'Event', 'FiducialE', 'OutE' ]
+    # In the output csv file, change InnerE -> FiducialE, FiducialE -> PartConE
+    columns = [ 'Run', 'Event', 'InnerE', 'FiducialE', 'OutE', 'TotalE', 'MaxE', 'TopCRTE', 'BottomCRTE', 
+               'FrontCRTE', 'BackCRTE', 'LeftCRTE', 'RightCRTE' ]
     odf = pd.DataFrame( columns = columns )
     
     # unit in mm, half the dimension
@@ -29,10 +31,14 @@ if __name__ == "__main__":
     TPCX = 300.
     TPCY = 250.
     TPCZ = 300.
+    # Inner volume
+    InX = 200.
+    InY = 150.
+    InZ = 200.
 
     for iFile in range( nFiles ):
 
-        infile = f'{indir}/{inPrefix}_g4_{iFile:02d}.root'
+        infile = f'{indir}/{inPrefix}_g4_{iFile:04d}.root'
         df = pd.DataFrame(ROOT.RDataFrame("edep", infile).AsNumpy())
 
         # Find the events and steps in TPC
@@ -41,6 +47,31 @@ if __name__ == "__main__":
         inTPC = df[isInTPC&( df.dE > 0. )]
 
         ByEvent = inTPC.groupby('event')
+
+        # Find the events and steps in cosmic ray taggers (CRT)
+        isInTopCRT = (((df.startZ<=635)&(df.endZ>=605))|((df.startZ>=605)&(df.endZ<=635)))&(df.startX.abs()<=500)& \
+                     (df.startY.abs()<=500)&(df.endX.abs()<=500)&(df.endY.abs()<=500)
+        inTopCRT = df[isInTopCRT&( df.dE > 0.)].groupby('event')
+
+        isInBottomCRT = (((df.startZ>=-635)&(df.endZ<=-605))|((df.startZ<=-605)&(df.endZ>=-635)))& \
+                        (df.startX.abs()<=500)&(df.startY.abs()<=500)&(df.endX.abs()<=500)&(df.endY.abs()<=500)
+        inBottomCRT = df[isInBottomCRT&( df.dE > 0.)].groupby('event')
+
+        isInFrontCRT = (((df.startY<=-505)&(df.endY>=-535))|((df.startY>=-535)&(df.endY<=-505)))& \
+                       (df.startX.abs()<=500)&(df.startZ.abs()<=600)&(df.endX.abs()<=500)&(df.endZ.abs()<=600)
+        inFrontCRT = df[isInFrontCRT&( df.dE > 0.)].groupby('event')
+
+        isInBackCRT = (((df.startY>=505)&(df.endY<=535))|((df.startY<=535)&(df.endY>=505)))& \
+                      (df.startX.abs()<=500)&(df.startZ.abs()<=600)&(df.endX.abs()<=500)&(df.endZ.abs()<=600)
+        inBackCRT = df[isInBackCRT&( df.dE > 0.)].groupby('event')
+
+        isInLeftCRT = (((df.startX<=-505)&(df.endX>=-535))|((df.startX>=-535)&(df.endX<=-505)))& \
+                      (df.startY.abs()<=500)&(df.startZ.abs()<=600)&(df.endY.abs()<=500)&(df.endZ.abs()<=600)
+        inLeftCRT = df[isInLeftCRT&( df.dE > 0.)].groupby('event')
+
+        isInRightCRT = (((df.startX>=505)&(df.endX<=535))|((df.startX<=535)&(df.endX>=505)))& \
+                       (df.startY.abs()<=500)&(df.startZ.abs()<=600)&(df.endY.abs()<=500)&(df.endZ.abs()<=600)
+        inRightCRT = df[isInRightCRT&( df.dE > 0.)].groupby('event')
 
         # Find events with an identifiable muon
         muonicEvents = []
@@ -58,13 +89,53 @@ if __name__ == "__main__":
         for eventNo, evt in inTPC.groupby('event'):
             if eventNo in muonicEvents: continue
             cs = evt[(evt.pdg.abs()==11)|(evt.pdg.abs()==13)|(evt.pdg.abs()==211)|(evt.pdg==2212)]
-            maskFi = (cs.startX.abs() <= FX)&(cs.startY.abs() <= FY)&(cs.startZ.abs() <= FZ)& \
+            maskIn = (cs.startX.abs() <= InX)&(cs.startY.abs() <= InY)&(cs.startZ.abs() <= InZ)& \
+                     (cs.endX.abs() <= InX)&(cs.endY.abs() <= InY)&(cs.endZ.abs() <= InZ)
+            maskFi = ~maskIn & (cs.startX.abs() <= FX)&(cs.startY.abs() <= FY)&(cs.startZ.abs() <= FZ)& \
                      (cs.endX.abs() <= FX)&(cs.endY.abs() <= FY)&(cs.endZ.abs() <= FZ)
-            maskOut = ~maskFi & (cs.startX.abs() <= TPCX)&(cs.startY.abs() <= TPCY)&(cs.startZ.abs() <= TPCZ)& \
+            maskOut = ~maskIn & ~maskFi & (cs.startX.abs() <= TPCX)&(cs.startY.abs() <= TPCY)&(cs.startZ.abs() <= TPCZ)& \
                      (cs.endX.abs() <= TPCX)&(cs.endY.abs() <= TPCY)&(cs.endZ.abs() <= TPCZ)
+            InnerE = cs[maskIn].dE.sum()
             FiducialE = cs[maskFi].dE.sum()
             OutE = cs[maskOut].dE.sum()
-            outCS = pd.DataFrame([{ 'Run': iFile, 'Event': eventNo, 'FiducialE': FiducialE, 'OutE': OutE }])
+            TotalE = InnerE + FiducialE + OutE
+            MaxE = cs.groupby('trackID').dE.sum().max()
+
+            # Check CRT hits
+            TopCRTE = 0.
+            BottomCRTE = 0.
+            FrontCRTE = 0.
+            BackCRTE = 0.
+            LeftCRTE = 0.
+            RightCRTE = 0.
+
+            if eventNo in inTopCRT.groups.keys():
+                crtEvt = inTopCRT.get_group(eventNo)
+                TopCRTE = crtEvt[(crtEvt.pdg.abs()==11)|(crtEvt.pdg.abs()==13)|(crtEvt.pdg.abs()==211)|(crtEvt.pdg==2212)].dE.sum()
+            
+            if eventNo in inBottomCRT.groups.keys():
+                crtEvt = inBottomCRT.get_group(eventNo)
+                BottomCRTE = crtEvt[(crtEvt.pdg.abs()==11)|(crtEvt.pdg.abs()==13)|(crtEvt.pdg.abs()==211)|(crtEvt.pdg==2212)].dE.sum()
+            
+            if eventNo in inFrontCRT.groups.keys():
+                crtEvt = inFrontCRT.get_group(eventNo)
+                FrontCRTE = crtEvt[(crtEvt.pdg.abs()==11)|(crtEvt.pdg.abs()==13)|(crtEvt.pdg.abs()==211)|(crtEvt.pdg==2212)].dE.sum()
+
+            if eventNo in inBackCRT.groups.keys():
+                crtEvt = inBackCRT.get_group(eventNo)
+                BackCRTE = crtEvt[(crtEvt.pdg.abs()==11)|(crtEvt.pdg.abs()==13)|(crtEvt.pdg.abs()==211)|(crtEvt.pdg==2212)].dE.sum()
+
+            if eventNo in inLeftCRT.groups.keys():
+                crtEvt = inLeftCRT.get_group(eventNo)
+                LeftCRTE = crtEvt[(crtEvt.pdg.abs()==11)|(crtEvt.pdg.abs()==13)|(crtEvt.pdg.abs()==211)|(crtEvt.pdg==2212)].dE.sum()
+            
+            if eventNo in inRightCRT.groups.keys():
+                crtEvt = inRightCRT.get_group(eventNo)
+                RightCRTE = crtEvt[(crtEvt.pdg.abs()==11)|(crtEvt.pdg.abs()==13)|(crtEvt.pdg.abs()==211)|(crtEvt.pdg==2212)].dE.sum()
+
+            outCS = pd.DataFrame([{ 'Run': iFile, 'Event': eventNo, 'InnerE': InnerE, 'FiducialE': FiducialE, 'OutE': OutE, 
+                                   'TotalE': TotalE, 'MaxE': MaxE, 'TopCRTE': TopCRTE, 'BottomCRTE': BottomCRTE, 'FrontCRTE': FrontCRTE,
+                                    'BackCRTE': BackCRTE, 'LeftCRTE': LeftCRTE, 'RightCRTE': RightCRTE }])
             odf = pd.concat([ odf, outCS ], ignore_index = True)
     
     odf.to_csv( outcsv, index = False)
